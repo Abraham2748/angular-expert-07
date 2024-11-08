8cbe11bdb2fd1ea45beac93c6f09f5d242c17b3f
Abraham2748 <abraham_jack10@hotmail.com>
2024-11-07 21:25:01 -0500

    Update title in app component to 'Clase 07 - Despliegue'

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular-expert-07' title`, () => {
+  it(`should have the 'Clase 07 - Despliegue' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-expert-07');
+    expect(app.title).toEqual('Clase 07 - Despliegue');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-expert-07');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, Clase 07 - Despliegue'
+    );
   });
 });
