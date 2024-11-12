11cf59ac0822c4eae50dca174aeb236ea48eac90
Abraham2748 <abraham_jack10@hotmail.com>
2024-11-12 01:24:50 -0500

    Refactor menuItems in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,15 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Clase 07 - Despliegue';
+
+  menuItems = [
+    { title: 'Explore the Docs', link: 'https://angular.dev' },
+    { title: 'Learn with Tutorials', link: 'https://angular.dev/tutorials' },
+    { title: 'CLI Docs', link: 'https://angular.dev/tools/cli' },
+    {
+      title: 'Angular Language Service',
+      link: 'https://angular.dev/tools/language-service',
+    },
+    { title: 'AngularDevTools', link: 'https://angular.dev/tools/devtools' },
+  ];
 }
